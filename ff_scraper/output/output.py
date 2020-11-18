6d792f7be29124e436d9aee495385b570099d9f7
Joe <joe.stone.se@gmail.com>
2020-11-18 14:04:50 -0500

    add docs to output

--- ff_scraper/output/output.py
+++ ff_scraper/output/output.py
@@ -1,4 +1,16 @@
 def write_output(file_name: str, outputs: list) -> None:
+    """ Writes each row in :outputs to :file_name.csv as its own line
+
+    Will write the CSV of each row of outputs to the provided file name.
+
+    Args
+        file_name (str): The name you want the csv file to have
+        outputs (list): A list of lists to be written as comma separated values
+            per line to file_name
+    
+    Returns
+        None
+    """
     with open(file_name + '.csv', 'w') as file:
         for output in outputs:
             if output:
